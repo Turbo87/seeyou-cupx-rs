2c82a3d32274a77110c4c84c9f77951c23fa4790
Tobias Bieniek <tobias@bieniek.cloud>
2025-10-05 12:46:54 +0200

    Add basic API draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use seeyou_cup::{CupEncoding, CupFile, Task, Waypoint};
+use std::io::{Read, Seek};
+use std::path::Path;
+
+pub struct CupxFile {
+    cup_file: CupFile,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl CupxFile {
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<(Self, Vec<Warning>), Error> {
+        todo!()
     }
+    pub fn from_path_with_encoding<P: AsRef<Path>>(
+        path: P,
+        encoding: CupEncoding,
+    ) -> Result<(Self, Vec<Warning>), Error> {
+        todo!()
+    }
+
+    pub fn from_reader<R: Read + Seek>(reader: R) -> Result<(Self, Vec<Warning>), Error> {
+        todo!()
+    }
+    pub fn from_reader_with_encoding<R: Read + Seek>(
+        reader: R,
+        encoding: CupEncoding,
+    ) -> Result<(Self, Vec<Warning>), Error> {
+        todo!()
+    }
+
+    pub fn cup_file(&self) -> &CupFile {
+        &self.cup_file
+    }
+
+    pub fn waypoints(&self) -> &[Waypoint] {
+        &self.cup_file().waypoints
+    }
+
+    pub fn tasks(&self) -> &[Task] {
+        &self.cup_file().tasks
+    }
+
+    // /// Get reader for image by filename (without "pics/" prefix)
+    // /// Returns error if image doesn't exist
+    // /// Only one image can be read at a time (requires &mut self)
+    // pub fn image(&mut self, filename: &str) -> Result<impl Read + '_, Error> {
+    //     todo!()
+    // }
+    //
+    // /// Iterator over all available image filenames (without "pics/" prefix)
+    // pub fn image_names(&self) -> impl Iterator<Item = String> {
+    //     todo!()
+    // }
+}
+
+#[derive(Debug, Clone)]
+pub struct Warning {
+    pub message: String,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    Zip(zip::result::ZipError),
+    Cup(seeyou_cup::Error),
+    MissingPointsCup,
+    ImageNotFound(String),
+    InvalidCupxStructure(String),
 }
